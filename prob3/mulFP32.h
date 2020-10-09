1080c36e97ea85c750f0af0cf3ee851a7630b500
Positron <42044624+MrPositron@users.noreply.github.com>
2020-10-09 18:01:37 +0500

    Add files via upload

---
+++ prob3/mulFP32.h
@@ -0,0 +1,6 @@
+#ifndef MULFP_32
+#define MULFP_32
+
+void multiplyFP32(float *A, float *B, float *C, int M, int N, int K);
+
+#endif
---
+++ prob3/mulINT32.h
@@ -0,0 +1,6 @@
+#ifndef MULINT_32
+#define MULINT_32
+
+void multiplyINT32(int32_t *A, int32_t *B, int32_t *C, int M, int N, int K);
+
+#endif
